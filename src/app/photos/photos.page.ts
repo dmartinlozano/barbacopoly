6fb26c9a98223df294fb7d49a733c0819e248055
David Martín Lozano <david.martin.lozano@gmail.com>
2018-11-10 00:11:55 +0100

    First version of camera

--- src/app/photos/photos.page.ts
+++ src/app/photos/photos.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Camera, CameraOptions} from '@ionic-native/camera/ngx';
 import {PhotosService} from './photos.service'
-import {Transfer, FileUploadOptions, TransferObject} from '@ionic-native/transfer';
+import { Router } from '@angular/router';
 import {ToastController, LoadingController} from '@ionic/angular';
 
 @Component({
@@ -17,7 +17,9 @@ export class PhotosPage implements OnInit {
   slice: number=30;
 
   constructor(private photosService: PhotosService,
-              private camera: Camera) { }
+              private camera: Camera,
+              private toastController: ToastController,
+              private router: Router) { }
 
   async ngOnInit() {
     this.list();
@@ -25,7 +27,8 @@ export class PhotosPage implements OnInit {
 
   async list(){
     const items = await this.photosService.list(this.isAsc, this.nextPageToken);
-    this.images = this.images.concat(items.items);
+    this.images = items.items;
+    //this.images = this.images.concat(items.items);
     this.nextPageToken = items.nextPageToken;
   }
 
@@ -47,7 +50,7 @@ export class PhotosPage implements OnInit {
   takePicture() {
     const options: CameraOptions = {
       quality: 100,
-      destinationType: this.camera.DestinationType.FILE_URI,
+      destinationType: this.camera.DestinationType.DATA_URL,
       sourceType: this.camera.PictureSourceType.CAMERA,
       encodingType: this.camera.EncodingType.JPEG,
       mediaType: this.camera.MediaType.PICTURE,
@@ -57,11 +60,22 @@ export class PhotosPage implements OnInit {
       correctOrientation: true
     };
     this.camera.getPicture(options).then(async(imageData) => {
-      const result = await this.photosService.post(imageData);
-      console.log(result);
-      
-     }, (err) => {
-      // Handle error
-     });
+      try{
+        await this.photosService.post(imageData);
+        let toast = await this.toastController.create({
+          message: "Foto subida a Internet",
+          duration: 2000
+        });
+        toast.present();
+      }catch(e){
+        let toast = await this.toastController.create({
+          message: "La foto no se ha podido subir a Internet",
+          duration: 2000
+        });
+        toast.present();
+      }finally{
+        this.router.navigateByUrl('/photos');
+      }
+    });
   }
 }
