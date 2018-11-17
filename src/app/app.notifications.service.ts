4dc32a574e6841962364a13fb79aabcac2edc2b7
David Martín Lozano <david.martin.lozano@gmail.com>
2018-11-17 14:01:21 +0100

    Mimi time commented

--- src/app/app.notifications.service.ts
+++ src/app/app.notifications.service.ts
@@ -7,13 +7,12 @@ export class NotificationsService {
     constructor(private localNotifications: LocalNotifications) { }
 
     load(){
-      // Schedule delayed notification
-      this.localNotifications.schedule({
+     /* this.localNotifications.schedule({
        text: 'Mimi Time!',
        trigger: {at: new Date(new Date().getTime() + 10)},
        led: 'FF0000',
        vibrate: true,
        sound: 'file://sound.mp3'
-      });
+      });*/
     }
 }
