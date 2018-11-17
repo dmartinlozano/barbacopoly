ca7bc81ee2c6376e014e32496ed09009036d2682
David Martín Lozano <david.martin.lozano@gmail.com>
2018-11-17 13:17:19 +0100

    Photos of meeting point in the middle of page

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -8,11 +9,20 @@ import { Router } from '@angular/router';
 })
 export class LoginPage  {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+              private menu: MenuController) { }
   login(){
     //if (this.password == "quepasarael12deEnero?"){
       this.router.navigateByUrl('/photos');
     //}
   }
+
+  ionViewDidEnter() {
+    this.menu.swipeEnable(false);
+  }
+
+  ionViewWillLeave() {
+    this.menu.swipeEnable(true);
+   }
 
 }
