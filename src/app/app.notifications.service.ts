27c7e3031bf4711c1a9a2a3efe7e63d8eea1e0ee
David Martín Lozano <david.martin.lozano@gmail.com>
2018-11-17 23:55:46 +0100

    - Logo fix - Notifications ok

--- src/app/app.notifications.service.ts
+++ src/app/app.notifications.service.ts
@@ -8,24 +8,17 @@ export class NotificationsService {
     constructor(private localNotifications: LocalNotifications) { }
 
     load(){
-     /* this.localNotifications.schedule({
-       text: 'Mimi Time!',
-       trigger: {at: new Date(new Date().getTime() + 10)},
-       led: 'FF0000',
-       vibrate: true,
-       sound: 'file://sound.mp3'
-      });*/
       activitiesJson.forEach(activity => {
         let hour = activity.time.split(':')[0];
         let minute = activity.time.split(':')[1];
         this.localNotifications.schedule({
           text: activity.name,
-          //trigger: { at: new Date(2019, 0, 12, hour, minute) },
-          trigger: { at: new Date(2018, 10, 17, hour, minute) },
+          trigger: { at: new Date(2019, 0, 12, hour, minute) },
+          //trigger: { at: new Date(2018, 10, 17, 23, 40) },
           led: 'FF0000',
-          vibrate: true//,
-          //icon: activity.icon,
-          //sound: '/asset/notification.mp3'
+          vibrate: true,
+          icon: 'warning',
+          sound: 'file://./../asset/notification.mp3'
          });
       });
     }
