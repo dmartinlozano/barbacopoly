6274f39847c63f21038808b4438b216ae6b57697
David Martín Lozano <david.martin.lozano@gmail.com>
2018-11-18 01:16:29 +0100

    Splash & icons changed

--- src/directives/image.loader.directive.ts
+++ src/directives/image.loader.directive.ts
@@ -6,16 +6,13 @@ import {  Directive, Renderer2, ElementRef, HostListener } from '@angular/core';
 export class ImageLoader {
 
   constructor(private renderer: Renderer2, private el: ElementRef) {
-      console.log("pasa por aqui 1");
-      this.renderer.setAttribute(this.el.nativeElement, 'src',  "/assets/loading.gif");
-    }
+    this.renderer.setAttribute(this.el.nativeElement, 'src',  "/assets/loading.gif");
+  }
 
   @HostListener('load') onLoad() {
-    console.log("pasa por aqui 2");
     this.renderer.setAttribute(this.el.nativeElement, 'src', this.el.nativeElement.src);
   }
   @HostListener('error') onError() {
-    console.log("pasa por aqui 3");
     this.renderer.setAttribute(this.el.nativeElement, 'src', "/assets/error.gif");
   }
 
