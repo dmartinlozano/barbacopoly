18314497a8e747cc50a0321c7c152878383b2574
David Martín Lozano <david.martin.lozano@gmail.com>
2018-12-26 06:46:41 +0100

    Video list storaged

--- src/app/videos/videos-upload/videos-upload.page.ts
+++ src/app/videos/videos-upload/videos-upload.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, EventEmitter } from '@angular/core';
 import { MediaFile } from '@ionic-native/media-capture/ngx';
 import { VideosService } from '../videos.service';
 import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
+import { NativeStorageService}  from '../../app.native.storage.service';
 
 export enum ProgressUpload{
   Wait = 0,
@@ -26,32 +27,54 @@ export class VideosUploadPage implements OnInit {
   subscriptionVideoUploaded: any;
 
   constructor(private videosService: VideosService,
-              private localNotifications: LocalNotifications) { }
+              private localNotifications: LocalNotifications,
+              private nativeStorageService: NativeStorageService) { }
 
-  ngOnInit() {
+  async ngOnInit() {
     var _self = this;
+    try{
+      this.videos = await this.nativeStorageService.getItem("videos");
+      this.videos.forEach(v => {
+          v.error = null;
+          v.state = 0;
+      });
+    }catch(e){
+      this.videos =[];
+    }
+
     this.subscriptionVideoToUpload = this.videosService.getFileUploading().subscribe( async function(newVideo: MediaFile){
-      _self.videos.push({file: newVideo, state: ProgressUpload.Wait, error: null});
+      _self.videos.unshift({file: newVideo, state: ProgressUpload.Wait, error: null});
+      await _self.nativeStorageService.setItem("videos", _self.videos);
     });
     this.subscriptionVideoUploaded = this.videosService.getFileUploaded().subscribe( async function(video: FileUpload){
+      
+      if (video.state === 3){
+        _self.localNotifications.schedule({
+          title: 'Barbacopoly',
+          text: 'Video subido. En breve lo publicaremos.',
+        });
+      }
+
+      if (video.error !== null){
+        _self.localNotifications.schedule({
+          title: 'Barbacopoly',
+          text: video.error.message,
+        });
+      }
+
       _self.videos.forEach(v => {
         if (v.file.fullPath === video.file.fullPath){
           v.error = video.error;
           v.state = video.state;
-          if (video.error !== null){
-            _self.localNotifications.schedule({
-              title: 'Barbacopoly',
-              text: video.error.message,
-            });
-          }else{
-            _self.localNotifications.schedule({
-              title: 'Barbacopoly',
-              text: 'Video subido.\nEn breve lo publicaremos.',
-            });
-          };
         };
       });
+      await _self.nativeStorageService.setItem("videos", _self.videos);
     });
+  }
+
+  async deleteAll(){
+    this.videos = [];
+    await this.nativeStorageService.setItem("videos", this.videos);
   }
   initUpload(video: FileUpload){
     this.videosService.postVideo(video);
