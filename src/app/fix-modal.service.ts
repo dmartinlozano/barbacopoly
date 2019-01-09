0e4b5b862a1f07737ab58c50e6a116a3a8ad256e
David Martín Lozano <david.martin.lozano@gmail.com>
2019-01-09 11:50:25 +0100

    Update fix-modal.service.ts

--- src/app/fix-modal.service.ts
+++ src/app/fix-modal.service.ts
@@ -1,24 +1,34 @@
 import { Injectable } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FixModalService {
 
-   constructor() {}
+  constructor(private actionSheetController: ActionSheetController, private alertController: AlertController) { }
 
-  async fix(){
+  async fix() {
+
+    //try dismiss without problems.
+    let actionSheetController = await this.actionSheetController.getTop();
+    if (actionSheetController) { try { actionSheetController.dismiss(); } catch (e) { } }
+
+    let alertController = await this.alertController.getTop();
+    if (alertController) { try { alertController.dismiss(); } catch (e) { } }
+
     //fix back actionsheets:
     let ionActionSheets = document.querySelectorAll('ion-action-sheet');
-    for (let i = 0; i< ionActionSheets.length; i++){
+    for (let i = 0; i < ionActionSheets.length; i++) {
       ionActionSheets[i].remove();
     }
     let ionActionSheetControllers = document.querySelectorAll('ion-action-sheet-controller');
-    for (let i = 0; i< ionActionSheetControllers.length; i++){
+    for (let i = 0; i < ionActionSheetControllers.length; i++) {
       ionActionSheetControllers[i].remove();
     }
     let ionAlert = document.querySelectorAll('ion-alert');
-    for (let i = 0; i< ionAlert.length; i++){
+    for (let i = 0; i < ionAlert.length; i++) {
       ionAlert[i].remove();
     }
   }
