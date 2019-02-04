e8ea3f5f7c891dde1a360dd376dbfef62bdbf3e8
David Martín Lozano <david.martin.lozano@gmail.com>
2019-02-04 11:27:15 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,24 +7,13 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CredentialsService} from './app.credentials.service';
-import { NotificationsService} from './app.notifications.service';
 import { NativeStorageService} from './app.native.storage.service';
-import { FixModalService} from './fix-modal.service';
 import { VideosUploadPage} from './videos/videos-upload/videos-upload.page';
 import { VideosViewPage } from './videos/videos-view/videos-view.page';
-
-//export function getCredentials(credentials: CredentialsService) {
-  //return () => credentials.load();
-//}
 
 export function initializerApp(credentials: CredentialsService, notifications: NotificationsService) {
   credentials.load();
   return () => notifications.load();
-}
-export function initializeFirebase(){
-  let credentialsService:CredentialsService = new CredentialsService();
-  credentialsService.load();
-  return credentialsService.credentials["firebase"];
 }
 
 @NgModule({
@@ -34,23 +23,16 @@ export function initializeFirebase(){
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
-    HttpClientModule,
-    AngularFireModule.initializeApp(initializeFirebase())
+    HttpClientModule
   ],
   providers: [
     File,
     HttpClient,
-    PhotoLibrary,
-    PhotoViewer,
-    NativeStorage,
     NativeStorageService,
     CredentialsService,
     NotificationsService,
-    UniqueDeviceID,
-    VideoPlayer,
     ScreenOrientation,
     MenuController,
-    FixModalService,
     { provide: APP_INITIALIZER, useFactory: initializerApp, deps: [CredentialsService, NotificationsService], multi: true },
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
